7c44fc97485936801563f2266815be64e96636ae
Dmitry Yermoshkin <spanb4@gmail.com>
2021-02-19 17:12:44 +0300

    feat: add (O) player

--- src/components/GameField/GameField.js
+++ src/components/GameField/GameField.js
@@ -4,15 +4,20 @@ import classes from './GameField.module.scss';
 
 export default function GameField() {
     const [cells, setCells] = useState(Array(9).fill(null));
+    const [xIsNext, setXisNext] = useState(true);
 
     function cellClickHandler(index) {
         const cellsCopy = cells.slice();
-        cellsCopy[index] = 'X';
+        cellsCopy[index] = xIsNext ? 'X' : 'O';
         setCells(cellsCopy);
+        setXisNext(!xIsNext);
     }
+
+    const status = `It's player ` + (xIsNext ? 'X' : 'O') + ' turn';
 
     return (
         <div className={classes.GameField}>
+            <div className={classes.Moves}>{status}</div>
             <div className={classes.CellsItems}>
                 {cells.map((cell, index) => {
                     return <Cell key={index} value={cells[index]} cellClickHandler={() => cellClickHandler(index)} />;
